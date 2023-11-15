544e46e3dc7c6565c9a5ca86bee5ae4ea59af493
Assem Talha <astalha@e2r6p6.1337.ma>
2023-11-15 10:53:49 +0100

    finishing cpp07

--- cpp07/ex02/Array.hpp
+++ cpp07/ex02/Array.hpp
@@ -9,49 +9,19 @@ class Array
     private:
 
         T   *array;
-        unsigned int size;
+        unsigned int _size;
 
     public:
 
-        Array()
-        {
-            size = 1;
-            array = new T[size];
-        };
-        Array(unsigned int n)
-        {
-            size = n;
-            this->array = new T[size];
-        };
-        Array(const Array& other)
-        {
-            *this = other;
-        };
-        Array& operator=(const Array& other)
-        {
-            size = other.size;
-            array = new T[other.size];
-            for (int i = 0; i < (int)other.size; i++)
-                array[i] = other.array[i];
-            return(*this);
-        }
-        R& T::operator[](S b)
-        {
-            
-        }
-        void     set_array(T content)
-        {
-            for (int i = 0; i < (int)size; i++)
-                array[i] = content;
-        }
-        void     get_array()
-        {
-            for (int i = 0; i < (int)size; i++)
-              std::cout <<  array[i] << ", ";
-            std::cout << std::endl;
-        }
-
-
+        Array();
+        ~Array();
+        Array(unsigned int n);
+        Array(const Array<T>& other);
+        Array<T>& operator=(const Array<T>& other);
+        T& operator[](int index);
+        unsigned int size() const;
 };
+
+#include "Array.tpp"
 
 #endif
---
+++ cpp07/ex02/Array.tpp
@@ -0,0 +1,50 @@
+#include "Array.hpp"
+
+template<typename T>
+Array<T>::Array()
+{
+    _size = 1;
+    array = new T[_size];
+}
+template<typename T>
+Array<T>::~Array()
+{
+    delete [] array;
+}
+
+template<typename T>
+Array<T>::Array(unsigned int n)
+{
+    _size = n;
+    this->array = new T[_size];
+}
+
+template<typename T>
+Array<T>::Array(const Array<T>& other)
+{
+    *this = other;
+}
+
+template<typename T>
+Array<T>& Array<T>::operator=(const Array<T>& other)
+{
+    _size = other._size;
+    array = new T[other._size];
+    for (int i = 0; i < (int)other._size; i++)
+        array[i] = other.array[i];
+    return(*this);
+}
+
+template<typename T>
+T& Array<T>::operator[](int index)
+{
+    if (index > (int)_size)
+        throw std::out_of_range("Index out of the range");
+    return(array[index]);
+}
+
+template<typename T>
+unsigned int Array<T>::size() const
+{
+    return (_size);
+}
