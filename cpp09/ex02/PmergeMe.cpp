3d5abf2ebd0886fb738b2e985c036e0f1f602225
Assem Talha <astalha@e2r2p6.1337.ma>
2023-12-21 21:51:18 +0100

    finishing cpp09

--- cpp09/ex02/PmergeMe.cpp
+++ cpp09/ex02/PmergeMe.cpp
@@ -1,34 +1,42 @@
 #include "PmergeMe.hpp"
 
-void      PmergeMe::print_vec()
-{
-
-    for (vc_iterator it = vc_seq.begin(); it < vc_seq.end(); it++)
-    {
-        std::cout << "[" << it->first << " , " << it->second << "]" << std::endl;
-    }
-    // for (dq_iterator it = dq_seq.begin(); it < dq_seq.end(); it++)
-    // {
-    //     std::cout << "[" << it->first << " , " << it->second << "]" << std::endl;
-    // }
-    if (strugler.first)
-        std::cout << strugler.second << std::endl;
-}
-
 void    validator(std::string input)
 {
     if (input.find_first_not_of("0123456789") != input.npos)
         throw (std::logic_error("Envalid input !!!"));
 }
 
-void   PmergeMe::parser(int ac, char **av)
+void      PmergeMe::print_Before(int ac, char **av)
+{
+    for (int i = 1; i < ac; i++)
+    {
+        validator(av[i]);
+    }
+    std::cout << "Before : ";
+    for (int i = 1; i < ac; i++)
+    {
+        std::cout << atoi(av[i]) << " ";
+    }
+    std::cout << std::endl;
+}
+
+void    PmergeMe::print_after()
+{
+    std::cout << "After : ";
+    for (std::vector<int>::iterator it = main_chain.begin(); it < main_chain.end(); it++)
+    {
+        std::cout << *it << " ";
+    }
+    std::cout << std::endl;
+}
+
+void   PmergeMe::vc_parser(int ac, char **av)
 {
     strugler.first = false;
     std::pair<int , int> pr;
 
     for (int i = 1; i < ac ; i += 2)
     {
-        validator(av[i]);
         if (!av[i + 1] && (ac - 1)%2)
             strugler = std::make_pair(true, atoi(av[i]));
         else
@@ -37,13 +45,31 @@ void   PmergeMe::parser(int ac, char **av)
             if (pr.first < pr.second)
                 std::swap(pr.first, pr.second);
             vc_seq.push_back(pr);
-            dq_seq.push_back(pr);
         }
     }
     std::sort(vc_seq.begin(), vc_seq.end());
+    vc_insertion();
+}
+
+void   PmergeMe::dq_parser(int ac, char **av)
+{
+    strugler.first = false;
+    std::pair<int , int> pr;
+
+    for (int i = 1; i < ac ; i += 2)
+    {
+        if (!av[i + 1] && (ac - 1)%2)
+            strugler = std::make_pair(true, atoi(av[i]));
+        else
+        {
+            pr = std::make_pair(atoi(av[i]), atoi(av[i + 1]));
+            if (pr.first < pr.second)
+                std::swap(pr.first, pr.second);
+            dq_seq.push_back(pr);
+        }
+    }
     std::sort(dq_seq.begin(), dq_seq.end());
-    insertion();
-    print_vec();
+    dq_insertion();
 } 
 
 unsigned int j(int n)
@@ -62,7 +88,7 @@ unsigned int j(int n)
     return (cur);
 }
 
-std::vector<int>    Jacobsthal_Generate(int size)
+std::vector<int>    PmergeMe::Jacobsthal_Generate(int size)
 {
     std::vector<int>    jacob;
     std::vector<int>    co_jacob;
@@ -85,7 +111,7 @@ std::vector<int>    Jacobsthal_Generate(int size)
     return (co_jacob);
 }
 
-std::vector<int>    PmergeMe::merge()
+std::vector<int>    PmergeMe::vc_merge()
 {
     std::vector<int> main_chain;
 
@@ -97,10 +123,23 @@ std::vector<int>    PmergeMe::merge()
     return (main_chain);
 }
 
-void    PmergeMe::insertion()
+std::deque<int>    PmergeMe::dq_merge()
 {
-    std::vector<int> Jacobsthal_num = Jacobsthal_Generate(vc_seq.size());
-    std::vector<int> main_chain = merge();
+    std::deque<int> main_chain;
+
+    main_chain.push_back(dq_seq.at(0).second);
+    for (dq_iterator it = dq_seq.begin(); it < dq_seq.end() ; it++)
+    {
+        main_chain.push_back(it->first);
+    }
+    return (main_chain);
+}
+
+void    PmergeMe::vc_insertion()
+{
+    Jacobsthal_num = Jacobsthal_Generate(vc_seq.size());
+    main_chain = vc_merge();
+
     std::vector<int>::iterator it;
 
     for(int i = 0; i < Jacobsthal_num.size() ; i++)
@@ -110,4 +149,36 @@ void    PmergeMe::insertion()
         it = std::lower_bound(main_chain.begin(), main_chain.end(), vc_seq.at(Jacobsthal_num[i] - 1).second);
         main_chain.insert(it, vc_seq.at(Jacobsthal_num[i] - 1).second );
     }
+    if (strugler.first)
+        main_chain.insert(std::lower_bound(main_chain.begin(), main_chain.end(), strugler.second), strugler.second);
+    // std::cout << std::is_sorted(main_chain.begin(), main_chain.end()) << std::endl;
+    // for (it = main_chain.begin(); it < main_chain.end(); it++)
+    // {
+    //     std::cout << "[" << *it << "]";
+    // }
+    // std::cout << std::endl;
+}
+
+void    PmergeMe::dq_insertion()
+{
+    // std::vector<int> Jacobsthal_num = Jacobsthal_Generate(dq_seq.size());
+    std::deque<int> main_chain1 = dq_merge();
+
+    std::deque<int>::iterator it;
+
+    for(int i = 0; i < Jacobsthal_num.size() ; i++)
+    {
+        if (Jacobsthal_num[i] - 1 >= dq_seq.size())
+            continue;
+        it = std::lower_bound(main_chain1.begin(), main_chain1.end(), dq_seq.at(Jacobsthal_num[i] - 1).second);
+        main_chain1.insert(it, dq_seq.at(Jacobsthal_num[i] - 1).second );
+    }
+    if (strugler.first)
+        main_chain1.insert(std::lower_bound(main_chain1.begin(), main_chain1.end(), strugler.second), strugler.second);
+    // std::cout << std::is_sorted(main_chain.begin(), main_chain.end()) << std::endl;
+    // for (it = main_chain.begin(); it < main_chain.end(); it++)
+    // {
+    //     std::cout << "[" << *it << "]";
+    // }
+    // std::cout << std::endl;
 }
--- cpp09/ex02/PmergeMe.hpp
+++ cpp09/ex02/PmergeMe.hpp
@@ -5,24 +5,102 @@
 #include <vector>
 #include <deque>
 #include <set>
+#include <chrono>
+#include <ctime>
+
+void    validator(std::string input);
 
 class PmergeMe
 {
     private:
         std::vector<std::pair<int, int> > vc_seq;
         std::deque<std::pair<int,int> >  dq_seq;
+        std::vector<int> Jacobsthal_num;
+        std::vector<int> main_chain;
         std::pair<bool, int> strugler;
      public:
         // ~PmergeMe();
+        std::vector<int>    Jacobsthal_Generate(int size);
         PmergeMe()
         {
 
         }
-        // PmergeMe(std::vector<std::pair<int, int>>);
-        void    parser(int ac, char **av);
-        void     print_vec();
-        void    insertion();
-        std::vector<int>    merge();
-        typedef std::vector<std::pair<int, int> >::iterator vc_iterator; 
-        typedef std::deque<std::pair<int, int> >::iterator dq_iterator; 
+        void vc_parser(int ac, char **av);
+        void dq_parser(int ac, char **av);
+        std::vector<int>    vc_merge();
+        std::deque<int>    dq_merge();
+        void    vc_insertion();
+        void    dq_insertion();
+        void     print_Before(int ac, char **av);
+        void     print_after();
+        typedef std::vector<std::pair<int, int> >::iterator vc_iterator;
+        typedef std::deque<std::pair<int, int> >::iterator dq_iterator;
 };
+
+
+
+
+
+    //     template <typename C, typename T>
+    //     T    merge(C Container)
+    //     {
+    //         T main_chain;
+    //         typename C::iterator it;
+
+    //         main_chain.push_back(Container.at(0).second);
+    //         for (it = Container.begin(); it < Container.end() ; it++)
+    //         {
+    //             main_chain.push_back(it->first);
+    //         }
+    //         return (main_chain);
+    //     }
+
+    //     template <typename C, typename T>
+    //     void    insertion(C Container)
+    //     {
+    //         std::vector<int> Jacobsthal_num = Jacobsthal_Generate(Container.size());
+    //         T main_chain = merge<C,T>(Container);
+
+    //         typename T::iterator it;
+
+    //         for(int i = 0; i < Jacobsthal_num.size() ; i++)
+    //         {
+    //             if (Jacobsthal_num[i] - 1 >= Container.size())
+    //                 continue;
+    //             it = std::lower_bound(main_chain.begin(), main_chain.end(), Container.at(Jacobsthal_num[i] - 1).second);
+    //             main_chain.insert(it, Container.at(Jacobsthal_num[i] - 1).second );
+    //         }
+    //         if (strugler.first)
+    //             main_chain.insert(std::lower_bound(main_chain.begin(), main_chain.end(), strugler.second), strugler.second);
+    //         std::cout << std::is_sorted(main_chain.begin(), main_chain.end()) << std::endl;
+    //         // for (it = main_chain.begin(); it < main_chain.end(); it++)
+    //         // {
+    //         //     std::cout << "[" << *it << "]";
+    //         // }
+    //         // std::cout << std::endl;
+    //     }
+
+    //     template <typename T, typename C>
+    //     void   parser(T Conatainer,int ac, char **av)
+    //     {
+    //         strugler.first = false;
+    //         std::pair<int , int> pr;
+
+    //         for (int i = 1; i < ac ; i += 2)
+    //         {
+    //             if (!av[i + 1] && (ac - 1)%2)
+    //                 strugler = std::make_pair(true, atoi(av[i]));
+    //             else
+    //             {
+    //                 validator(av[i]);
+    //                 validator(av[i + 1]);
+    //                 pr = std::make_pair(atoi(av[i]), atoi(av[i + 1]));
+    //                 if (pr.first < pr.second)
+    //                     std::swap(pr.first, pr.second);
+    //                 Conatainer.push_back(pr);
+    //             }
+    //         }
+    //     std::sort(Conatainer.begin(), Conatainer.end());
+    //     insertion<T,C>(Conatainer);
+    // // print_vec();
+    //     } 
